bed3dd0b1024b997be7e0f31624b64011ffc91cb
vamshiballikonda <vamshiballikonda@gmail.com>
2016-05-17 11:31:52 +0530

    New things added

--- sample.py
+++ sample.py
@@ -3,7 +3,8 @@ with open('links.txt') as f:
     link = [[int(x) for x in line.split( )] for line in f ]
 with open('buses.txt') as f:
     bus = [[int(x) for x in line.split( )] for line in f ]
-bus1=bus[:]
+import copy
+bus1 = copy.deepcopy(bus)
 b = [i[2] for i in bus]
 u = [i for i,x in enumerate(b) if x>0] #generator bus index
 v = [j for j,x in enumerate(b) if x<0] #load bus index
@@ -12,12 +13,21 @@ v1 = [i+1 for i in v] #all load bus numbers
 gen = [bus[i-1] for i in u] #all generator bus values
 load = [bus[i-1] for i in v] #all load bus values
 p=[[i+1 for i,x in enumerate (link[j]) if x==1] for j in u] #links to generator buses
-q=p[:] # q should be change but p should remain constant so that I can use it later. But p is also changing.
-for i,x in enumerate (q):
+import copy
+q = copy.deepcopy(p)
+ # q is a copy of p which are links to generator buses
+for i,x in enumerate (q): # links to generator buses which are not generators
 	for ux in u1:
 		try:
 			p1=x.index(ux)
 			del q[i][p1]
 		except:
 			pass
-
+j=1
+for i in u: # assigning islands to generators and the nearest max loads 
+    bus1[i][3]=j
+    #print range(q[j-1])
+    #for k in range(len(q[j-1])
+        #print q[j-1][k]
+        #print k
+    #j=j+1
