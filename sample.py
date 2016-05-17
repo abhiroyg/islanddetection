eb5a160232d502bde8ee32485d886dfe1fc17ae1
vamshiballikonda <vamshiballikonda@gmail.com>
2016-05-17 10:09:05 +0530

    Add files via upload
    The value p is getting changed when I am changing q. Should'nt it just copy if my code is q=p[:] but the value of p is also  changing when we are changing the value of q.

--- sample.py
+++ sample.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python
 with open('links.txt') as f:
     link = [[int(x) for x in line.split( )] for line in f ]
-#linking matrix has been uploaded
-#positive for
 with open('buses.txt') as f:
     bus = [[int(x) for x in line.split( )] for line in f ]
+bus1=bus[:]
 b = [i[2] for i in bus]
-#c=b[:]
-#c.sort(reverse=True) #ordering based on weights
 u = [i for i,x in enumerate(b) if x>0] #generator bus index
 v = [j for j,x in enumerate(b) if x<0] #load bus index
 u1 = [i+1 for i in u] #all generator bus numbers
@@ -15,11 +12,13 @@ v1 = [i+1 for i in v] #all load bus numbers
 gen = [bus[i-1] for i in u] #all generator bus values
 load = [bus[i-1] for i in v] #all load bus values
 p=[[i+1 for i,x in enumerate (link[j]) if x==1] for j in u] #links to generator buses
-for i,x in enumerate (p): #finding out buses which are connected to the generator bus but arent generators
+q=p[:]
+for i,x in enumerate (q):
 	for ux in u1:
 		try:
 			p1=x.index(ux)
-			del p[i][p1]
+			del q[i][p1]
 		except:
 			pass
-
+print p
+print q
